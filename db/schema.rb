7706e6011d4487cb1981758f94b486dd561fb9bb
Justas Kazakauskas <justas.kazakauskas@gmail.com>
2023-03-16 17:34:22 +0200

    Add docker-compose improvements

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_12_12_212520) do
 
-  create_table "records", id: { type: :integer, unsigned: true }, charset: "utf8mb4", force: :cascade do |t|
+  create_table "records", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "signature", null: false
     t.integer "version", null: false, unsigned: true
     t.datetime "created_at", precision: 6, null: false
