eb39e7ed08b18fa7ddf33580d42c3704f327cb5b
Justas Kazakauskas <justas.kazakauskas@gmail.com>
2023-04-24 14:41:36 +0300

    Add updates

--- config/initializers/app.rb
+++ config/initializers/app.rb
@@ -2,6 +2,6 @@ module App
   module_function
 
   def redis
-    @redis ||= Redis.new(host: ENV['REDIS_HOST'], port: 6379)
+    @redis ||= Redis.new(host: ENV.fetch('REDIS_HOST', '0.0.0.0'), port: 6379)
   end
 end
