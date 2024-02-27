6a13594d8ba0388a11f053454e938bff18753643
Justas Kazakauskas <justas.kazakauskas@gmail.com>
2024-02-27 19:08:11 +0200

    Add some updates

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,2 +1,14 @@
 class StatsController < ApplicationController
+  def status
+    @hostname = Socket.gethostname
+    @schema_status = schema_status
+  end
+
+  private
+
+  def schema_status
+    Record.count && 'available'
+  rescue
+    'unavailable'
+  end
 end
