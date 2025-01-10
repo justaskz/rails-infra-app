30618918facbcb0001cca4facbd6564eab1e3080
Justas Kazakauskas <justas.kazakauskas@gmail.com>
2025-01-11 01:25:13 +0200

    Update Dockerfile

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,4 +1,13 @@
 Rails.application.configure do
   config.lograge.enabled = true
   config.lograge.formatter = Lograge::Formatters::KeyValue.new
+
+  config.lograge.custom_options = lambda do |event|
+    {
+      method: event.payload[:method],
+      path: event.payload[:path],
+    }
+  end
+
+  config.lograge.ignore_keys = ['format']
 end
